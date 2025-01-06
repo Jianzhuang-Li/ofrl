fe56af84787be31810d24b96d6b81702fd9b20f9
Jianzhuang-Li <2637197670@qq.com>
2025-01-06 22:08:04 +0800

    fix diffusion actor

--- fisher_brc/diffusion_ac.py
+++ fisher_brc/diffusion_ac.py
@@ -1,10 +1,11 @@
 from diffusion.models import Model_Cond_Diffusion, Model_mlp_diff_embed
-from diffusion.conf import hand_door_conf
+from diffusion.conf import hand_door_conf, hopper_conf
 from diffusion.distribution import MutivariateGuassion
+from conf.hopper import hopper_diffusion_conf
 import torch 
 import torch.utils.data as Data
-
-EXP_NAME = "diffusion"
+import gymnasium as gym
+import numpy as np
 
 class DiffusionActor(torch.nn.Module):
 
@@ -14,25 +15,47 @@ class DiffusionActor(torch.nn.Module):
         self.model_save = conf_.model_save
         self.nn_conf = conf_.nn_model_conf
         self.device = conf_.device  # conf-device
-        self.min_action = torch.tensor(-1, device="cuda")
-        self.max_action = torch.tensor(1, device="cuda")
+        self.model_load = conf_.model_load
+        self.min_action = torch.tensor(conf_.min_action, device=self.device)
+        self.max_action = torch.tensor(conf_.max_action, device=self.device)
+        self.model_val = conf_.model_val
         self.eps = 1e-6
         # self.fix_log_sigma = torch.ones((1,), device=self.device)
-        self.fix_log_sigma = torch.tensor(2.0, device=self.device)
+        self.fix_log_sigma = torch.tensor(conf_.fix_log_sigma, device=self.device)
         self.nn_model = Model_mlp_diff_embed(self.nn_conf.x_shape, self.nn_conf.n_hidden, self.nn_conf.y_dim, self.nn_conf.embed_dim).to(self.device)
         self.model = Model_Cond_Diffusion(self.nn_model, self.conf.model_conf)
-        self.load_check(300)
+        self.load_check()
+        self.model.eval() # TODO  不是eval模式的话，实测效果差？
 
-    def load_check(self, batch_check):
-        path = self.model_save.save_path + f"{EXP_NAME}/" + f"batch_{batch_check}_" +  self.model_save.file_name
+    def load_check(self):
+        path = self.model_load.exp_path + f"/batch_{self.model_load.batch_num}_" +  self.model_save.file_name
         self.model.load_state_dict(torch.load(path))
-        print(f"load checkpoint {batch_check} from {path}")
+        print(f"load checkpoint {self.model_load.batch_num} from {path}")
+
+    def sample(self, states):
+        # x_eval_r = states.repeat(2, 1)
+        if not isinstance(states, torch.Tensor):
+            states = torch.tensor(states, device=self.device)
+        if len(states.shape) == 1:
+            x_eval_r = states.unsqueeze(0)
+        else:
+            x_eval_r = states
+        if self.model_val.extra_diffusion_steps == 0:
+            y_predict = self.model.sample(x_eval_r)
+        else:
+            y_predict = self.model.sample_extra(x_eval_r, extra_steps=self.model_val.extra_diffusion_steps)
+        if len(states.shape) == 1:
+            sampled_actions = y_predict[0]
+        else:
+            sampled_actions = y_predict
+        sampled_actions = sampled_actions.clamp(self.min_action + self.eps, self.max_action - self.eps)
+        sampled_actions = sampled_actions.detach().cpu().numpy()
+        return sampled_actions
 
     def log_prob(self, states, actions, log_sigma=None, need_entropy=False):
         if log_sigma is None:
             log_sigma = self.fix_log_sigma
-        sampled_actions = self.model.sample(x_batch=states)
-        sampled_actions = sampled_actions.clamp(self.min_action + self.eps, self.max_action - self.eps)
+        sampled_actions = self.sample(states=states)
         dist = MutivariateGuassion(mu=sampled_actions, log_sigma=log_sigma)
         actions = actions.clamp(self.min_action + self.eps, self.max_action - self.eps)
         log_prob_ = dist.log_prob(val=actions).unsqueeze(-1)
@@ -44,20 +67,18 @@ class DiffusionActor(torch.nn.Module):
        
 
 if __name__ == "__main__":
-    m = DiffusionActor(hand_door_conf.conf_hand_door).to("cuda")
-    conf_ = hand_door_conf.conf_hand_door
-    dataset = conf_.data_specific.data_class(conf_.data_specific.data_conf)
-    dataset_loader = Data.DataLoader(dataset=dataset,
-                                    batch_size=conf_.data_specific.batch_size,
-                                    shuffle=conf_.data_specific.shuffle,
-                                    num_workers=conf_.data_specific.num_workers,
-                                    )
-    i = 0
-    for batch_data in dataset_loader:
-        
-        states, actions = batch_data
-        states = states.to(m.device)
-        actions = actions.to(m.device)
+    actor = DiffusionActor(conf_=hopper_diffusion_conf)
+    env = gym.make(hopper_conf.env.env_name, render_mode = hopper_conf.env.render_mode)
+    obs, _ = env.reset()
+
+    for i in range(500):
+        # ac = env.action_space.sample()
+        obs = obs.astype(np.float32)
         with torch.no_grad():
-            log_prob, entropy = m.log_prob(states, actions, need_entropy=True)
-        i += 1
+            obs = torch.tensor(obs, device=hopper_diffusion_conf.device)
+            ac = actor.sample(obs)
+        obs, rew, done, ter, info = env.step(ac)
+        env.render()
+        if done or ter:
+            break
+    env.close()
